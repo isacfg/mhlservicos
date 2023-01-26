e0e271daf40d2ee972d5c170f09a3fe8575ce395
isacfg <isaac.lima.105@ufrn.edu.br>
2023-01-26 13:04:09 -0300

    basic file structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screns: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
